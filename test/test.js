58720637d9d71bd2388e84e52b5002a6abd91d32
neekey <ni184775761@gmail.com>
2013-01-28 13:35:43 +0800

    添加对于windows的兼容性

--- test/test.js
+++ test/test.js
@@ -1,11 +1,46 @@
 var PS = require( '../index' );
+var CP = require( 'child_process' );
+var assert = require( 'assert' );
 
-PS.lookup( { pid: 10623 }, function( err, list ){
+var serverPath = './server_for_test.js';
+var child = CP.fork( serverPath );
+var pid = child.pid;
 
-    console.log( err, list );
+describe('test', function(){
 
-    PS.lookup( { command: 'node', arguments: 'run.mac' }, function( err, list ){
+    describe( '#lookup()', function(){
 
-        console.log( err, list );
+        it( 'by id', function( done ){
+            PS.lookup({ pid: String( pid ) }, function( err, list ){
+                assert.equal( list.length, 1 );
+                assert.equal( list[0].arguments[0], serverPath );
+
+                done();
+            });
+        });
+
+
+        it( 'by command & arguments', function( done ){
+            PS.lookup({ command: '.*node', arguments: serverPath }, function( err, list ){
+                assert.equal( list.length, 1 );
+                assert.equal( list[0].pid, pid );
+                assert.equal( list[0].arguments[0], serverPath );
+                done();
+            });
+        });
+    });
+
+    describe( '#kill()', function(){
+
+        it( 'kill', function( done ){
+
+            PS.kill( pid, function( err ){
+                assert.equal( err, null );
+                PS.lookup( { pid: String( pid ) }, function( err, list ){
+                    assert.equal( list.length, 0 );
+                    done();
+                });
+            });
+        });
     });
 });
