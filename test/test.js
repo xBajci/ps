1456ca158a910f77d6329f1587a105ae0343a3b3
neekey <ni184775761@gmail.com>
2013-01-28 15:46:57 +0800

    update

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ describe('test', function(){
 
 
         it( 'by command & arguments', function( done ){
-            PS.lookup({ command: '.*node', arguments: serverPath }, function( err, list ){
+            PS.lookup({ command: '.*node.*', arguments: serverPath }, function( err, list ){
                 assert.equal( list.length, 1 );
                 assert.equal( list[0].pid, pid );
                 assert.equal( list[0].arguments[0], serverPath );
