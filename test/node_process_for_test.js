188e149ddaf3aae45b8fdf4058d539608486103b
neekey <ni184775761@gmail.com>
2016-10-16 20:57:47 +1100

    add .editorconfig and reformat code

--- test/node_process_for_test.js
+++ test/node_process_for_test.js
@@ -1,7 +1,7 @@
 var now = Date.now();
-console.log( '[child] child process start!' );
+console.log('[child] child process start!');
 
-setInterval(function(){
-    var interval = Date.now() - now;
-    console.log( '[child] Timer finished, time consuming: ' + interval );
+setInterval(function () {
+  var interval = Date.now() - now;
+  console.log('[child] Timer finished, time consuming: ' + interval);
 }, 50);
--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-var PS = require( '../index' );
-var CP = require( 'child_process' );
-var assert = require( 'assert' );
-var Path = require( 'path' );
+var PS = require('../index');
+var CP = require('child_process');
+var assert = require('assert');
+var Path = require('path');
 
-var serverPath = Path.resolve( __dirname, './node_process_for_test.js' );
+var serverPath = Path.resolve(__dirname, './node_process_for_test.js');
 var UpperCaseArg = '--UPPER_CASE';
 var child = null;
 var pid = null;
 
 function startProcess() {
-  child = CP.fork(serverPath, [ UpperCaseArg ]);
+  child = CP.fork(serverPath, [UpperCaseArg]);
   pid = child.pid;
 }
 
-describe('test', function(){
+describe('test', function () {
   before(function (done) {
     PS.lookup({arguments: 'node_process_for_test'}, function (err, list) {
       var processLen = list.length;
@@ -35,77 +35,77 @@ describe('test', function(){
     });
   });
 
-    describe( '#lookup()', function(){
+  describe('#lookup()', function () {
 
-        it( 'by id', function( done ){
-            PS.lookup({ pid: String( pid ) }, function( err, list ){
-                assert.equal( list.length, 1 );
-                assert.equal( list[0].arguments[0], serverPath );
+    it('by id', function (done) {
+      PS.lookup({pid: String(pid)}, function (err, list) {
+        assert.equal(list.length, 1);
+        assert.equal(list[0].arguments[0], serverPath);
 
-                done();
-            });
-        });
-
-        it( 'by command & arguments', function( done ){
-            PS.lookup({ command: '.*(node|iojs).*', arguments: 'node_process_for_test' }, function( err, list ){
-                assert.equal( list.length, 1 );
-                assert.equal( list[0].pid, pid );
-                assert.equal( list[0].arguments[0], serverPath );
-                done();
-            });
-        });
-
-        it( 'by arguments, the matching should be case insensitive ', function( done ){
-            PS.lookup({ arguments: 'UPPER_CASE' }, function( err, list ){
-                assert.equal( list.length, 1 );
-                assert.equal( list[0].pid, pid );
-                assert.equal( list[0].arguments[0], serverPath );
-
-                PS.lookup({ arguments: 'upper_case' }, function( err, list ){
-                    assert.equal( list.length, 1 );
-                    assert.equal( list[0].pid, pid );
-                    assert.equal( list[0].arguments[0], serverPath );
-                    done();
-                });
-            });
-        });
-
-        it( 'empty result list should be safe ', function( done ){
-            PS.lookup({ command: 'NOT_EXIST', psargs: 'l' }, function( err, list ){
-                assert.equal( list.length, 0 );
-                done();
-            });
-        });
-
-        it('should work correctly with options `aux`', function(done) {
-            PS.lookup({ command: 'node', psargs: 'aux' }, function( err, list ){
-                assert.equal(list.length > 0, true);
-                list.forEach(function(row) {
-                    assert.equal(/^\d+$/.test(row.pid), true);
-                });
-                done();
-            });
-        });
+        done();
+      });
     });
 
-    describe( '#kill()', function(){
-
-        it( 'kill', function( done ){
-
-            PS.kill( pid, function( err ){
-                assert.equal( err, null );
-                PS.lookup( { pid: String( pid ) }, function( err, list ){
-                    assert.equal( list.length, 0 );
-                    done();
-                });
-            });
-        });
-
-        it( 'should not throw an exception if the callback is undefined', function( done ){
-            assert.doesNotThrow(function () {
-                PS.kill(pid);
-                setTimeout(done, 400);
-            });
-        });
+    it('by command & arguments', function (done) {
+      PS.lookup({command: '.*(node|iojs).*', arguments: 'node_process_for_test'}, function (err, list) {
+        assert.equal(list.length, 1);
+        assert.equal(list[0].pid, pid);
+        assert.equal(list[0].arguments[0], serverPath);
+        done();
+      });
     });
+
+    it('by arguments, the matching should be case insensitive ', function (done) {
+      PS.lookup({arguments: 'UPPER_CASE'}, function (err, list) {
+        assert.equal(list.length, 1);
+        assert.equal(list[0].pid, pid);
+        assert.equal(list[0].arguments[0], serverPath);
+
+        PS.lookup({arguments: 'upper_case'}, function (err, list) {
+          assert.equal(list.length, 1);
+          assert.equal(list[0].pid, pid);
+          assert.equal(list[0].arguments[0], serverPath);
+          done();
+        });
+      });
+    });
+
+    it('empty result list should be safe ', function (done) {
+      PS.lookup({command: 'NOT_EXIST', psargs: 'l'}, function (err, list) {
+        assert.equal(list.length, 0);
+        done();
+      });
+    });
+
+    it('should work correctly with options `aux`', function (done) {
+      PS.lookup({command: 'node', psargs: 'aux'}, function (err, list) {
+        assert.equal(list.length > 0, true);
+        list.forEach(function (row) {
+          assert.equal(/^\d+$/.test(row.pid), true);
+        });
+        done();
+      });
+    });
+  });
+
+  describe('#kill()', function () {
+
+    it('kill', function (done) {
+
+      PS.kill(pid, function (err) {
+        assert.equal(err, null);
+        PS.lookup({pid: String(pid)}, function (err, list) {
+          assert.equal(list.length, 0);
+          done();
+        });
+      });
+    });
+
+    it('should not throw an exception if the callback is undefined', function (done) {
+      assert.doesNotThrow(function () {
+        PS.kill(pid);
+        setTimeout(done, 400);
+      });
+    });
+  });
 });
